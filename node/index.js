d244200dd4f81f3df1aba5a7fd86185e8115d20f
Arpan Bhattacharya <apubhattacharya71@gmail.com>
2023-06-23 22:28:36 +0530

    committed

--- node/index.js
+++ node/index.js
@@ -9,6 +9,23 @@ mongoose.connect(
   "mongodb+srv://billing:arpan007@cluster0.jxzqgep.mongodb.net/billingdb?retryWrites=true&w=majority"
 );
 
+
+
+// Your code
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+// Your code
+
+
 const customer = require("./routes/customer");
 const product = require("./routes/product");
 const billing = require("./routes/bill");
